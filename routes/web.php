873518fcade731042dc4b3ec2c362ec43ac4b79d
trisnawahyudiii <wayantrisna79@gmail.com>
2023-07-06 01:47:39 +0800

    tambah halaman trisna

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function(){
-    
+Route::get('/hello', function(){ 
     return view('hello');
+});
+
+Route::get('/trisna', function(){ 
+    return view('trisna');
 });
